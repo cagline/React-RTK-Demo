3588164982b74b58809b55605656957acff39930
chanukag <chanukag@zone24x7.com>
2023-03-02 14:16:37 +0530

    Add todoApiSlice to the getDefaultMiddleware

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,14 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
+import {apiSlice as todoApiSlice} from '../api/todoApiSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [todoApiSlice.reducerPath] : todoApiSlice.reducer,
   },
+  middleware: getDefaultMiddleware =>
+      getDefaultMiddleware().concat(todoApiSlice.middleware)
 });
 
 export type AppDispatch = typeof store.dispatch;
